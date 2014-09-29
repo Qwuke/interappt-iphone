297ebddbce9636e57423fc16fdea81d7ccd8b217
rsnorman <rsnorman15@gmail.com>
2014-09-29 07:35:33 -0400

    Added search input bar to LocationSearchController

--- InterAPPt/LocationSearchController.swift
+++ InterAPPt/LocationSearchController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class LocationSearchController: UIViewController {
+class LocationSearchController: UIViewController, UISearchBarDelegate {
     var currentUser: User!
+    var searchField: UISearchBar!
     
     override func viewDidLoad() {
         self.currentUser = User.current()
-        
+        addNavigationBar()
+        addSearchField()
+    }
+    
+    func addNavigationBar() {
         self.title = "InterAPPt"
         self.navigationController.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Mohave-Bold", size: 24), NSForegroundColorAttributeName: UIColor(red: 167/255, green: 224/255, blue: 9/255, alpha: 1)]
         
@@ -26,5 +31,24 @@ class LocationSearchController: UIViewController {
         
         var profileButton: UIBarButtonItem = UIBarButtonItem(customView: profileImage)
         self.navigationItem.leftBarButtonItem = profileButton;
+    }
+    
+    func addSearchField() {
+        self.searchField = UISearchBar(frame: CGRectMake(0, 0, 300, 60))
+        self.searchField.center = self.view.center
+        self.searchField.placeholder = "Search Locations…"
+        self.searchField.searchBarStyle = UISearchBarStyle.Minimal;
+        self.searchField.delegate = self
+        self.view.addSubview(self.searchField)
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar!) {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.searchField.frame = CGRectMake(0, self.navigationController.navigationBar.frame.size.height + 20, self.view.frame.size.width, 40)
+        })
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
+        
     }
 }
